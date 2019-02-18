3332663a372eba55087ab759e51f083883f188aa
frederic charest <frederic.charest.42@hotmail.com>
2019-02-18 15:47:39 -0500

    validations and dependencies for models Animal, Booking and Review done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :animals
+  has_many :reviews
+  has_many :bookings
+
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :email, presence: true
 end
