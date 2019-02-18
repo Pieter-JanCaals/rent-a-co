f3bf20e255ece46e7325081bf98eda1f9391a8ec
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-18 15:47:02 -0500

    Ran db:migrate and removed email from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@ ANIMAL_TYPES = ["Bear", "Monkey", "Dolphin", "Zebra", "Elephant", "Lion", "Snake
   User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
     owner: owner
   )
 end
