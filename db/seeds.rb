f77049e185425a5d870ff2d5586252323295f73e
Jerome Kastoun <jeromekastoun@gmail.com>
2019-02-19 14:10:45 -0500

    removed a few buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@ end
     description: Faker::Hipster.paragraph,
     user: users[rand(0...users.size)],
     price: Faker::Number.decimal(2),
-    photo: "http://loremflickr.com/animals"
+    photo: "http://loremflickr.com/360/300/animals"
   )
 end
 
