69c939544cda886898cf2f3dd03ec0a33ca0e5cf
Jerome Kastoun <jeromekastoun@gmail.com>
2019-02-19 10:52:54 -0500

    added show page for animal and animals.scss

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  def create
+    @animal = Animal.find(params[:animal_id])
+    @review = Review.new(review_params)
+    @review.animal = @animal
+    if @review.save
+      redirect_to animal_path(@animal)
+    else
+      render "animals/show"
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
