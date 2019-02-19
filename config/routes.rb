43b5d46ae5d03974323ea220e670284a74ee0d0e
Jerome Kastoun <jeromekastoun@gmail.com>
2019-02-19 10:22:53 -0500

    added reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'animals#index'
-  resources :animals
+  resources :animals do
+    resources :reviews, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
