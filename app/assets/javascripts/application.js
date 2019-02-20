9bffee19f4ee89612518fc547c3ad71f6204502d
Jerome Kastoun <jeromekastoun@gmail.com>
2019-02-20 13:43:36 -0500

    added confirmation message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-
-$(window).load(function () {
-    $(".trigger_popup_fricc").click(function(){
-       $('.hover_bkgr_fricc').show();
-    });
-    $('.hover_bkgr_fricc').click(function(){
-        $('.hover_bkgr_fricc').hide();
-    });
-    $('.popupCloseButton').click(function(){
-        $('.hover_bkgr_fricc').hide();
-    });
-});
