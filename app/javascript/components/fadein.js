22155276017ccb272b56a135af8e4d7ad6d00615
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-21 15:09:17 -0500

    Fixed js on index page

--- app/javascript/components/fadein.js
+++ app/javascript/components/fadein.js
@@ -1,32 +1,24 @@
 const fadein = () => {
-  $(function(){  // $(document).ready shorthand
-    console.log("Fading in")
-    $('.first_elements').animate({'opacity':'1'},1500);
-  });
+  // $(function(){  // $(document).ready shorthand
+  //   $('.first_elements').animate({'opacity':'1'},1500);
+  // });
+
+  const renderCard = () => {
+    $('.hideme').each( function(item){
+        var top_of_object = $(this)[0].getBoundingClientRect().top;
+
+        /* If the object is visible for 50px in the window, fade it it */
+        if( $(window).height() - 50 > top_of_object ){
+          $(this).animate({'opacity':'1'},1500);
+        }
+
+    });
+  }
 
   $(document).ready(function() {
-
+    renderCard();
       /* Every time the window is scrolled ... */
-      $(window).scroll( function(){
-
-          /* Check the location of each desired element */
-          $('.hideme').each( function(i){
-              var bottom_of_object = $(this).position().top + $(this).outerHeight();
-              var bottom_of_window = $(window).scrollTop() + $(window).height();
-              console.log(`Bottom of object: ${bottom_of_object}, Bottom of window: ${bottom_of_window}`);
-              console.log($(this).offsetTop);
-              console.log($(window));
-              /* If the object is completely visible in the window, fade it it */
-              if( bottom_of_window > bottom_of_object ){
-
-                  $(this).animate({'opacity':'1'},1500);
-
-              }
-
-          });
-
-      });
-
+      $(window).scroll(renderCard);
   });
 };
 
