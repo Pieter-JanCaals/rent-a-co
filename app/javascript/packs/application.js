1ec5dd78418b8ad823d2316f141550f132b217a7
Jerome Kastoun <jeromekastoun@gmail.com>
2019-02-21 11:43:21 -0500

    added datepicker gem - added images to assets - configured booking controller to use params for date - edited the show animal page and the index bookings pages - imported to .js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,16 @@
 import "bootstrap";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { fadein } from '../components/fadein';
+import "bootstrap-datepicker"
+import "bootstrap-datepicker/dist/css/bootstrap-datepicker"
 initUpdateNavbarOnScroll();
 fadein();
+$('#booking_start_date').datepicker({
+  format: 'dd/mm/yyyy',
+    startDate: '-3d'
+    });
+$('#booking_end_date').datepicker({
+  format: 'dd/mm/yyyy',
+    startDate: '-3d'
+    });
+
